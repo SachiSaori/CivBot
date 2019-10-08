8efe2f753bee35d16e3a7c1ef1dc897d361b1813
Sofia Levacova <zhant@inbox.ru>
2019-10-08 23:48:56 +0300

    start coding in connection module

---
+++ MySQLDB.py
@@ -0,0 +1,6 @@
+import mysql.connector
+
+dbconfig = { 'host': 'eu-cdbr-west-02.cleardb.net',
+             'user': 'bca611917ce10f',
+             'password': '6f307489',
+             'database': 'heroku_4e4d24d83d2a5ae', }
